0f1e0179a97c1c42a2f06022b315ad2a996e7396
Sumit Kumar <kumarsumit8210@gmail.com>
2023-04-07 03:23:29 +0530

    Added pagination support

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -13,7 +13,15 @@ export class ProductListComponent implements OnInit {
 
     products: Product[] = [];
     currentCategoryId: number = 1;
+    previousCategoryId: number = 1;
     searchMode: boolean = false;
+
+    // new properties for pagination
+    pageNumber: number = 1;
+    pageSize: number = 10;
+    totalElements: number = 0;
+
+    previousKeyword: string = "";
 
     constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
@@ -25,41 +33,70 @@ export class ProductListComponent implements OnInit {
 
     listProducts() {
         this.searchMode = this.route.snapshot.paramMap.has('keyword');
-        if(this.searchMode) {
+        if (this.searchMode) {
             this.handleSearchProducts();
         } else {
             this.handleListProducts();
         }
     }
-    
+
     handleSearchProducts() {
         const keyword = this.route.snapshot.paramMap.get('keyword')!;
 
+        // if we have different keyword than previous
+        // then set the page number to 1
+        if (this.previousKeyword != keyword) {
+            this.pageNumber = 1;
+        }
+        this.previousKeyword = keyword;
+
         // now search for products using keyword
-        this.productService.searchProducts(keyword).subscribe(
-            data => {
-                this.products = data;
-            }
-        );
+        this.productService.searchProductsPaginate(this.pageNumber-1, this.pageSize, keyword)
+            .subscribe(this.processResult());
     }
-    
+
     handleListProducts() {
         // check if id parameter is available
         const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-        if(hasCategoryId) {
+        if (hasCategoryId) {
             // get the id param string. Use + to convert it to number
             this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
         } else {
             this.currentCategoryId = 1;
         }
-        console.log(this.currentCategoryId);
-        
-        this.productService.getProductList(this.currentCategoryId).subscribe(
-            data => {
-                this.products = data;
-            }
-        )
+
+        // check if we have a different category id than previous 
+        // Angular will reuse a component if it is currently being viewed
+
+        // if we have different category id than previous 
+        // then set page number back to 1
+        if (this.previousCategoryId != this.currentCategoryId) {
+            this.pageNumber = 1;
+        }
+
+        this.previousCategoryId = this.currentCategoryId;
+
+        // now get the products
+        this.productService.getProductListPaginate(
+            this.pageNumber - 1,
+            this.pageSize,
+            this.currentCategoryId
+        ).subscribe(this.processResult());
     }
 
+    updatePageSize(thePageSize: string) {
+        this.pageSize = +thePageSize;
+        this.pageNumber = 1;
+        this.listProducts();
+    }
+    
+    processResult() {
+        return (data: any) => {
+            this.products = data._embedded.products;
+            this.pageNumber = data.page.number + 1;
+            this.pageSize = data.page.size;
+            this.totalElements = data.page.totalElements;
+        };
+    }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,29 +11,53 @@ import { ProductCategory } from '../common/product-category';
 export class ProductService {
 
     private baseUrl = 'http://localhost:8080/api/products';
-    
+
     private categoryUrl = 'http://localhost:8080/api/product-category'
 
     constructor(private httpClient: HttpClient) { }
-    
+
     getProduct(theProductId: number): Observable<Product> {
         const productUrl = `${this.baseUrl}/${theProductId}`;
-        
+
         return this.httpClient.get<Product>(productUrl);
+    }
+
+    getProductListPaginate(
+        thePage: number,
+        thePageSize: number,
+        theCategoryId: number
+    ): Observable<GetResponseProducts> {
+
+        const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+            + `&page=${thePage}&size=${thePageSize}`;
+
+        return this.httpClient.get<GetResponseProducts>(searchUrl);
     }
 
     getProductList(theCategoryId: number): Observable<Product[]> {
         const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-        
+
         return this.getProducts(searchUrl);
     }
-    
+
     searchProducts(theKeyword: string): Observable<Product[]> {
         const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
-        
+
         return this.getProducts(searchUrl);
     }
-    
+
+    searchProductsPaginate(
+        thePage: number,
+        thePageSize: number,
+        theKeyword: string
+    ): Observable<GetResponseProducts> {
+
+        const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+            + `&page=${thePage}&size=${thePageSize}`;
+
+        return this.httpClient.get<GetResponseProducts>(searchUrl);
+    }
+
     private getProducts(searchUrl: string): Observable<Product[]> {
         return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
             map(response => response._embedded.products)
@@ -41,7 +65,7 @@ export class ProductService {
     }
 
     getProductCategories() {
-        
+
         return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
             map(response => response._embedded.productCategory)
         )
@@ -51,6 +75,12 @@ export class ProductService {
 interface GetResponseProducts {
     _embedded: {
         products: Product[];
+    },
+    page: {
+        size: number,
+        totalElements: number,
+        totalPages: number,
+        number: number
     }
 }
 
